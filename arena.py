3bbcd0e6d47ebd66738d7c6ad8bdfca56aa8979f
Christian M. T. Takagi <tkgcmt@gmail.com>
2014-10-25 15:30:57 -0200

    Merge remote-tracking branch 'origin/Bird&Formulas'

--- arena.py
+++ arena.py
@@ -1,180 +1,212 @@
-import pokemon as pkm
+import movimento
+import pokemon
 import random
+import os
 
 class Arena:
 
-   tabela = [
-   #  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14
-   [ 1.0,1.0,1.0,1.0,1.0,0.5,1.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0 ],# 0 - Normal
-   [ 2.0,1.0,0.5,0.5,1.0,2.0,0.5,0.0,1.0,1.0,1.0,1.0,0.5,2.0,1.0 ],# 1 - Fight
-   [ 1.0,2.0,1.0,1.0,1.0,0.5,2.0,1.0,1.0,1.0,2.0,0.5,1.0,1.0,1.0 ],# 2 - Flying
-   [ 1.0,1.0,1.0,0.5,0.5,0.5,2.0,0.5,1.0,1.0,2.0,1.0,1.0,1.0,1.0 ],# 3 - Poison
-   [ 1.0,1.0,0.0,2.0,1.0,2.0,0.5,1.0,2.0,1.0,0.5,2.0,1.0,1.0,1.0 ],# 4 - Ground
-   [ 1.0,0.5,2.0,1.0,0.5,1.0,2.0,1.0,2.0,1.0,1.0,1.0,1.0,2.0,1.0 ],# 5 - Rock
-   [ 1.0,0.5,0.5,2.0,1.0,1.0,1.0,0.5,0.5,1.0,2.0,1.0,2.0,1.0,1.0 ],# 6 - Bug
-   [ 0.0,1.0,1.0,1.0,1.0,1.0,1.0,2.0,1.0,1.0,1.0,1.0,0.0,1.0,1.0 ],# 7 - Ghost
-   [ 1.0,1.0,1.0,1.0,1.0,0.5,2.0,1.0,0.5,0.5,2.0,1.0,1.0,2.0,0.5 ],# 8 - Fire
-   [ 1.0,1.0,1.0,1.0,2.0,2.0,1.0,1.0,2.0,0.5,0.5,1.0,1.0,1.0,0.5 ],# 9 - Water
-   [ 1.0,1.0,0.5,0.5,2.0,2.0,0.5,1.0,0.5,2.0,0.5,1.0,1.0,1.0,0.5 ],#10 - Grass
-   [ 1.0,1.0,2.0,1.0,0.0,1.0,1.0,1.0,1.0,2.0,0.5,0.5,1.0,1.0,0.5 ],#11 - Electr
-   [ 1.0,2.0,1.0,2.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,0.5,1.0,1.0 ],#12 - Psychc
-   [ 1.0,1.0,2.0,1.0,2.0,1.0,1.0,1.0,1.0,0.5,2.0,1.0,1.0,0.5,2.0 ],#13 - Ice
-   [ 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.0 ] #14 - Dragon
-            ]
+   tabela = [               #Bird
+   #  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+   [ 1.0,1.0,1.0,1.0,1.0,0.5,1.0,1.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0 ], # 0 - Normal
+   [ 2.0,1.0,0.5,0.5,1.0,2.0,1.0,0.5,0.0,1.0,1.0,1.0,1.0,0.5,2.0,1.0 ], # 1 - Fight
+   [ 1.0,2.0,1.0,1.0,1.0,0.5,1.0,2.0,1.0,1.0,1.0,2.0,0.5,1.0,1.0,1.0 ], # 2 - Fly
+   [ 1.0,1.0,1.0,0.5,0.5,0.5,1.0,2.0,0.5,1.0,1.0,2.0,1.0,1.0,1.0,1.0 ], # 3 - Poison
+   [ 1.0,1.0,0.0,2.0,1.0,2.0,1.0,0.5,1.0,2.0,1.0,0.5,2.0,1.0,1.0,1.0 ], # 4 - Ground
+   [ 1.0,0.5,2.0,1.0,0.5,1.0,1.0,2.0,1.0,2.0,1.0,1.0,1.0,1.0,2.0,1.0 ], # 5 - Rock
+   [ 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0 ], # 6 - Bird
+   [ 1.0,0.5,0.5,2.0,1.0,1.0,1.0,1.0,0.5,0.5,1.0,2.0,1.0,2.0,1.0,1.0 ], # 7 - Bug
+   [ 0.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.0,1.0,1.0,1.0,1.0,0.0,1.0,1.0 ], # 8 - Ghost
+   [ 1.0,1.0,1.0,1.0,1.0,0.5,1.0,2.0,1.0,0.5,0.5,2.0,1.0,1.0,2.0,0.5 ], # 9 - Fire
+   [ 1.0,1.0,1.0,1.0,2.0,2.0,1.0,1.0,1.0,2.0,0.5,0.5,1.0,1.0,1.0,0.5 ], # 10 - Water
+   [ 1.0,1.0,0.5,0.5,2.0,2.0,1.0,0.5,1.0,0.5,2.0,0.5,1.0,1.0,1.0,0.5 ], # 11- Grass
+   [ 1.0,1.0,2.0,1.0,0.0,1.0,1.0,1.0,1.0,1.0,2.0,0.5,0.5,1.0,1.0,0.5 ], # 12- Eletr
+   [ 1.0,2.0,1.0,2.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,0.5,1.0,1.0 ], # 13- Psychc
+   [ 1.0,1.0,2.0,1.0,2.0,1.0,1.0,1.0,1.0,1.0,0.5,2.0,1.0,1.0,0.5,2.0 ], # 14- Ice
+   [ 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.0 ]  # 15- Dragon
+             ]
 
    def base_damage ( self, pokeA, atk, pokeB ):
       termo1 = ( 2 * pokeA.get_level () + 10 ) / 250
       termo2 = pokeA.get_atk () / pokeB.get_def () * atk.get_power ()
-      print ( '# DEBUG base_damage = ', ( termo1 * termo2 + 2 ) )
       return ( termo1 * termo2 + 2 )
 
-   def stab ( self, poke, atk ):
-      if poke.get_type1 () == atk.get_type ():
-         print ( '# DEBUG stab = 1.5' )
+   def stab ( self, pokemon, movimento ):
+      if pokemon.get_type1 () == movimento.get_type ():
          return 1.5
-      if poke.get_type2 () == atk.get_type ():
-         print ( '# DEBUG stab = 1.5' )
+      elif pokemon.get_type2 () == movimento.get_type ():
          return 1.5
-      print ( '# DEBUG stab = 1.0' )
-      return 1.0
+      else:
+         return 1
 
-   def type_effect ( self, atk, poke ):
-      effect = self.tabela[atk.get_type ()][poke.get_type1 ()]
-      if poke.get_type2 () != 15:
-         print ( '# DEBUG  entra', poke.get_type2 () )
-         print ( '# DEBUG tabela =',
-                   self.tabela[ atk.get_type () ][ poke.get_type2 () ] )
-         effect = effect * self.tabela[atk.get_type ()][poke.get_type2 ()]
-      print ( '# DEBUG effect =', effect )
+   def type_effect ( self, atk, pokemon ):
+      effect = self.tabela [atk.get_type ()][pokemon.get_type1 ()]
+      if pokemon.get_type2 () != 16:
+         effect = effect * self.tabela [atk.get_type ()][pokemon.get_type2 ()]
+      if effect > 1:
+         print ( 'Ataque super efetivo' )
+      elif effect < 1:
+         print ( 'Ataque nao muito efetivo' )
       return effect
 
-   def critical ( self ):
-      if random.random () >= 0.9:
-         print ( '# DEBUG Critical = 2' )
-         return 2
+   def critical ( self, pokemon ):
+      chance = pokemon.get_spd () / 512
+      rand   = random.random ()
+      if rand <= chance:
+         lvl = pokemon.get_level ()
+         print ( 'Ataque critico' )
+         return ( ( 2 * lvl ) / ( lvl + 5 ) )
       else:
-         print ( '# DEBUG Critical = 1' )
          return 1
 
    def rand ( self ):
-      num = random.uniform ( 0.85, 1 )
-      print ( '# DEBUG rand =', num )
-      return num 
+      return random.uniform ( 0.85, 1 )
 
-   def modifier_damage ( self, pokeA, mov, pokeB ):
-      termo1 = self.stab( pokeA, mov ) * self.type_effect ( mov, pokeB )
-      termo2 = self.critical () * self.rand ()
-      print ( '# DEBUG modifier_damage =', ( termo1 * termo2 ) )
+   def modifier_damage ( self, pokeA, atk, pokeB ):
+      termo1 = self.stab ( pokeA, atk ) * self.type_effect ( atk, pokeB )
+      termo2 = self.critical ( pokeA ) * self.rand ()
       return ( termo1 * termo2 )
 
-   def damage ( self, pokeA, mov, pokeB ):
-      dano = self.base_damage ( pokeA, mov, pokeB )
-      dano = dano * self.modifier_damage ( pokeA, mov, pokeB )
-      print ( '# DEBUG dano =', dano )
+   def damage ( self, pokeA, atk, pokeB ):
+      dano = self.base_damage ( pokeA, atk, pokeB )
+      dano = dano * self.modifier_damage ( pokeA, atk, pokeB )
       return int ( dano )
 
-   def __init__ ( self ):
-      print ( '--- Player 1 ---' )
-      self.poke1 = pkm.Pokemon ()
-      print ( '--- Player 2 ---' )
-      self.poke2 = pkm.Pokemon ()
-      self.contador = 0
-      self.batalha ()
-
-   def batalha ( self ):
-      print ( '--- Inicio da Batalha ---' )
-      while self.rodada () == True:
-         pass
-
-      print ( '--- Fim da Batalha ---' )
-      print ( 'Batalha terminou no turno:', self.contador )
-      if self.poke1.esta_vivo:
-         print ( 'Vitoria do Player 1' )
-      else:
-         print ( 'Vitoria do Player 2' )
-
-   def rodada ( self ):
-      if self.poke1.get_spd () > self.poke2.get_spd ():
-         primeiro = self.poke1
-         segundo  = self.poke2
-      else:
-         primeiro = self.poke2
-         segundo  = self.poke1
-
-      self.contador = self.contador + 1
-
-      if primeiro.esta_vivo () and segundo.esta_vivo ():
-         self.turno ( primeiro, segundo )
+   def accuracy ( self, mov ):
+      accu = mov.get_accu ()
+      if accu > 1:
+         accu = accu * 0.01
+      rand = random.random ()
+      if rand <= accu:
+         return True
       else:
          return False
 
-      if primeiro.esta_vivo () and segundo.esta_vivo ():
-         self.turno ( segundo, primeiro )
-      else:
-         return False
-
-      return True
-
-   def mostra_info_baixo ( self, poke1, poke2 ):
+   def mostra_info_turno ( self, poke1, poke2 ):
       print ()
-      print ( '### Turno', self.contador, '###' )
+      print ( ' Turno #', self.contador )
+      print ( )
+      print ( '                              +---' )
+      print ( '                              | Nome:', poke2.get_name () )
+      print ( '                              | HP:', ( poke2.get_hp () /
+                                             poke2.get_hpmax () ) * 100, '%' )
+      print ( '                              +---' )
       print ()
-      print ( '\  +---' )
-      print ( ' \ | Nome:', poke1.get_name () )
-      print ( ' / | HP:', poke1.get_hp (), '/', poke1.get_hpmax () )
-      print ( '/  +---' )
-      print ()
-      print ( '     +--+'   )
-      print ( '     |  |'   )
-      print ( '   +-+  +-+' )
-      print ( '    \    /'  )
-      print ( '     \  /'   )
-      print ( '      \/'    )
-      print ()
-      print ( '   +---' )
-      print ( '   | Nome:', poke2.get_name () )
-      print ( '   | HP:', poke2.get_hp (), '/', poke2.get_hpmax () )
-      print ( '   +---' )
+      print ( ' +---' )
+      print ( ' | Nome:', poke1.get_name () )
+      print ( ' | HP:', ( poke1.get_hp () / poke1.get_hpmax () ) * 100, '%' )
+      print ( ' +---' )
+      print ( )
 
-   def mostra_informacoes ( self, poke ):
-      print ()
-      print ( '+---' )
-      print ( '| Noome:', poke.get_name () )
-      print ( '| HP:', poke.get_hp (), '/', poke.get_hpmax () )
-      print ( '+---' )
-      print ()
+   def mostra_info_pokemon ( self, poke ):
+      print ( )
+      print ( ' +---' )
+      print ( ' | Nome:', poke.get_name () )
+      print ( ' | HP:', poke.get_hp (), '/', poke.get_hpmax () )
+      print ( ' +---' )
+      print ( )
 
-   def turno ( self, ataque, defesa ):
-      move_struggle = False
-      self.mostra_info_turno ( ataque, defesa )
 
-      if ataque.possui_ataques ():
-         ataque.mostra_ataques ()
+   def escolhe_ataques ( self, poke ):
+      self.mostra_info_pokemon ( poke )
+      poke.mostra_ataques ()
 
+      if poke.possui_ataques ():
          while ( True ):
-            opt = int ( input ( 'Qual ataque executar? ' ) )
-            if opt >= 0 and opt < ataque.get_numAtk ():
-               if ataque.moveset[opt].get_pp () < 1:
-                  print ( 'No moves left' )
+            opt = int ( input ( 'Qual ataque deseja executar?' ) )
+            if opt >= 0 and opt < poke.get_numAtk ():
+               atk = poke.moveset[opt]
+               if atk.get_pp () < 1:
+                  print ( 'Sem movimentos restantes' )
                else:
                   break
             else:
                print ( 'ERRO: Ataque invalido' )
       else:
-         move_struggle = True
-         print ( ataque.get_name (), 'nao possui mais ataques.' )
-         opt = ataque.get_numAtk ()
+         print ( poke.get_name (), 'nao possui mais ataques.' )
+         opt = poke.get_numAtk ()
 
-      # calcula e aplica o dano ao defensor e mostra suas informacoes
-      atk = ataque.moveset[opt]
-      dano = self.damage ( ataque, atk, defesa )
-      defesa.set_hp ( defesa.get_hp () - dano )
-      self.mostra_informacoes ( defesa )
+      return opt
 
-      if move_struggle and defesa.esta_vivo ():
-         # pelo que entendi o atacante so sofre recoil se o
-         # defensor estiver vivo
-         print ( ataque.get_name (), 'is damaged by recoil' )
-         dano = int ( dano * 0.5 )
-         ataque.set_hp ( ataque.get_hp - dano )
-         self.mostra_informacoes ( ataque )
+   def realiza_ataque ( self, ataque, numero, defesa ):
+      movimento = ataque.moveset[ numero ]
+
+      print ( ataque.get_name (), 'usou', movimento.get_name () )
+
+      if self.accuracy ( movimento ):
+         dano = self.damage ( ataque, movimento, defesa )
+         porcentagem = dano / defesa.get_hp () * 100
+         print ( defesa.get_name (), 'perdeu', porcentagem, '% da sua vida' )
+         defesa.set_hp ( defesa.get_hp () - dano )
+
+         if numero == ataque.get_numAtk (): # Struggle
+            print ( ataque.get_name (), 'sofre com o recoil' )
+            dano = int ( dano / 2 )
+            porcentagem = dano / ataque.get_hp () * 100
+            print ( ataque.get_name (), 'perdeu', porcentagem, '% da sua vida' )
+            ataque.set_hp ( ataque.get_hp () - dano )
+      else:
+         print ( ataque.get_name (), 'errou o ataque' )
+
+      movimento.reduz_pp ()
+
+   def turno ( self, poke1, poke2 ):
+      self.contador = self.contador + 1
+      print ( '# Player 1' )
+      self.mostra_info_turno ( poke1, poke2 )
+      ataque1 = self.escolhe_ataques ( poke1 )
+
+      os.system ( 'clear' )
+
+      print ( '# Player 2' )
+      self.mostra_info_turno ( poke1, poke2 )
+      ataque2 = self.escolhe_ataques ( poke2 )
+
+      os.system ( 'clear' )
+      self.mostra_info_turno ( poke1, poke2 )
+      if poke1.get_spd () > poke2.get_spd ():
+         primeiro = poke1
+         first    = ataque1
+         segundo  = poke2
+         second   = ataque2
+      else:
+         primeiro = poke2
+         first    = ataque2
+         segundo  = poke1
+         second   = ataque1
+
+      if primeiro.esta_vivo () and segundo.esta_vivo ():
+         self.realiza_ataque ( primeiro, first, segundo )
+      else:
+         return False
+
+      if primeiro.esta_vivo () and segundo.esta_vivo ():
+         self.realiza_ataque ( segundo, second, primeiro )
+      else:
+         return False
+
+      if primeiro.esta_vivo () and segundo.esta_vivo ():
+         return True
+      else:
+         return False
+
+   def __init__ ( self ):
+      print ( ' Player 1 ' )
+      self.pkmn1 = pokemon.Pokemon ()
+      os.system ( 'clear' )
+      print ( ' Player 2 ' )
+      self.pkmn2 = pokemon.Pokemon ()
+      os.system ( 'clear' )
+      self.contador = 0
+
+      while ( self.turno ( self.pkmn1, self.pkmn2 ) ):
+         input ( 'Pressione qualquer tecla para iniciar o proximo turno' )
+
+      os.system ( 'clear' )
+      print ( '   Fim da Batalha' )
+      self.mostra_info_turno ( self.pkmn1, self.pkmn2 )
+      if self.pkmn1.esta_vivo ()== False and self.pkmn2.esta_vivo () == False:
+         print ( ' EMPATE' )
+      elif self.pkmn1.esta_vivo ():
+         print ( ' PLAYER 1 VENCEU' )
+      else:
+         print ( ' PLAYER 2 VENCEU' )
